2b36d3e40a8cdb2d335ba43a454217fd0169c74d
ibrahimroshdy <ibrahimabdelrahman@aucegypt.edu>
2022-06-25 01:05:34 +0200

    [REF-16] [MODIFIED] README.md

--- scripts/local_run.sh
+++ scripts/local_run.sh
@@ -9,14 +9,12 @@ echo "Running makemigrations and migrate..."
 python3 manage.py makemigrations
 python3 manage.py migrate
 
-#echo "Running elastic search index..."
-#echo "y" | python3 manage.py search_index --rebuild
 
 echo "Loading Fixtures..."
 python3 manage.py loaddata django_celery_fixtures
 
 echo "Creating Superuser..."
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); print('   USER EXISTS....') if User.objects.filter(username='admin').exists() else User.objects.create_superuser('admin', 'fileyy@email.com', 'admin') " | python3 manage.py shell
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); print('Admin User Exists....') if User.objects.filter(username='admin').exists() else User.objects.create_superuser('admin', 'test@test.com', 'admin') " | python3 manage.py shell
 
 echo "Collecting static.."
 echo "yes" | python3 manage.py collectstatic
