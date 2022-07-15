10b88cf0cf1bd99749370e0c9539cf02abe889d5
ibrahimroshdy <ibrahimabdelrahman@aucegypt.edu>
2022-07-15 14:09:13 +0200

    [REF-25] [MODIFIED] ForeignKeyWidget fixes

--- apps/speedtester/admin.py
+++ apps/speedtester/admin.py
@@ -7,10 +7,10 @@ from . import models, resources
 
 @admin.register(models.SpeedtesterModel)
 class SpeedtesterAdmin(ImportExportModelAdmin, ExportActionMixin):  # pragma: no cover
-    list_display = ['best_server', 'download', 'upload', 'created']
+    list_display = ['best_server', 'client', 'download', 'upload', 'created']
     list_filter = ['best_server__country', 'best_server__name', 'best_server__cc']
-    search_fields = ['best_server__country', 'best_server__name', 'best_server__cc']
-    readonly_fields = ['best_server','client']
+    search_fields = ['best_server__country', 'best_server__name', 'best_server__cc', 'best_server__url', 'client__isp']
+    readonly_fields = ['best_server', 'client']
     date_hierarchy = 'created'
     resource_class = resources.SpeedtesterResource
 
@@ -19,7 +19,7 @@ class SpeedtesterAdmin(ImportExportModelAdmin, ExportActionMixin):  # pragma: no
 class ServerAdmin(ImportExportModelAdmin, ExportActionMixin):  # pragma: no cover
     list_display = ['url', 'created', 'name', 'country', 'cc', 'sponsor', 'host']
     list_filter = ['name', 'country', 'cc']
-    search_fields = ['name', 'country', 'cc']
+    search_fields = ['name', 'country', 'cc', 'url']
     date_hierarchy = 'created'
 
 
